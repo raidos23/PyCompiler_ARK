78031a1b19f0ae22d9410564896d4b1ec32b3b99
Samuel Amen Ague <ague.samuel27@gmail.com>
2025-12-12 23:20:42 +0100

    mise au point de l'architecture du SDK

---
+++ Plugins_SDK/UiContext/i18n.py
@@ -0,0 +1,1 @@
+# module pour i18naliser les Plugins(Ce module est extremement en relation avec ./Core/i18n.py)
---
+++ Plugins_SDK/__init__.py
@@ -0,0 +1,1 @@
+# SDK pour Plugins (BC et AC) couvrant les contextes paticuliers et generaux
