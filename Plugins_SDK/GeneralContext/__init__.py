b79a15f7be7e4e946825bdd47fe4539cfbff3593
Samuel Amen Ague <ague.samuel27@gmail.com>
2025-12-14 21:36:03 +0100

    R

--- Plugins_SDK/GeneralContext/__init__.py
+++ Plugins_SDK/GeneralContext/__init__.py
@@ -1,4 +1,4 @@
 from .Dialog import Dialog as Dialog
-from .i18n import apply_translations as translation
+from .i18n import apply_translations as translation_for_plugins
 
-__all__ = ["Dialog", "translation"]
+__all__ = ["Dialog", "translation_for_plugins"]
