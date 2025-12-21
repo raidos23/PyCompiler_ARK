9bc046d56abeb4990095fae496d1c865da8c97af
Samuel Amen Ague <ague.samuel27@gmail.com>
2025-12-21 15:30:10 +0100

    Amelioration de l'SDK

--- Plugins_SDK/AcPluginContext/Context.py
+++ Plugins_SDK/AcPluginContext/Context.py
@@ -1,1 +1,4 @@
 # extension de capacité pour les plugins AC pour une meilleur affinité avec le logiciel et ainsi avoir plus de possibilité
+from acasl import AcPluginBase, PostCompileContext, PluginMeta
+
+__all__ = ["AcPluginBase", "PostCompileContext", "PluginMeta"]
