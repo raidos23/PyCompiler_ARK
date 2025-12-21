12414f32e0a4855e5409443c708017fdf2afaefd
Samuel Amen Ague <ague.samuel27@gmail.com>
2025-12-21 17:06:16 +0100

    amaeliaraotion de sdk dialg

--- Plugins_SDK/GeneralContext/Logging.py
+++
@@ -1,9 +0,0 @@
-class Logging:
-    def log_info(message: str) -> None:
-        print(f"[INFO] {message}")
-
-    def log_warn(message: str) -> None:
-        print(f"[WARN] {message}")
-
-    def log_error(message: str) -> None:
-        print(f"[ERROR] {message}")
