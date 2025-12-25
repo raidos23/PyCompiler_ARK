5852fb133de5b95c1d57933a457ff47ebf41951b
Samuel Amen Ague <ague.samuel27@gmail.com>
2025-12-25 15:44:04 +0100

    mise a jour de la licnece vers apache

--- Core/deps_analyser/__init__.py
+++ Core/deps_analyser/__init__.py
@@ -1,3 +1,18 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Ague Samuel Amen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Analyse des dépendances Python pour PyCompiler ARK++.
 Inclut la détection, la suggestion et l'installation automatique des modules manquants.
