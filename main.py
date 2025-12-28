ad95f7367671745698085433052e60c22148d7c3
Ague Samuel Amen <ague.samuel27@gmail.com>
2025-12-28 12:57:09 +0100

    Add pycompiler_ark.py alternative entry point for launching the application

--- main.py
+++ main.py
@@ -450,7 +450,4 @@ def main(argv: list[str]) -> int:
 
 
 if __name__ == "__main__":
-    import multiprocessing
-
-    multiprocessing.set_start_method("spawn", force=True)
     sys.exit(main(sys.argv))
