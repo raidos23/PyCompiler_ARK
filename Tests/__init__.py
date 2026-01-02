70a98b6a96c87511f3f6841ac0c2fa00c9006c30
Samuel Amen Ague <ague.samuel27@gmail.com>
2026-01-02 09:40:41 +0100

    mise a jour de lentete de licence

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,5 +1,5 @@
 ﻿# SPDX-License-Identifier: Apache-2.0
-# Copyright 2025 Ague Samuel Amen
+# Copyright 2026 Ague Samuel Amen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,3 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
--- Tests/test_bcasl_config.py
+++ Tests/test_bcasl_config.py
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env python3
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2025 Ague Samuel Amen
+# Copyright 2026 Ague Samuel Amen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
