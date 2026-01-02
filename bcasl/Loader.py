ea318e408cca01fd885ef0435e62d5028aea1b72
Samuel Amen Ague <ague.samuel27@gmail.com>
2026-01-02 09:40:41 +0100

    mise a jour de lentete de licence

--- bcasl/Loader.py
+++ bcasl/Loader.py
@@ -1,5 +1,5 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2025 Ague Samuel Amen
+﻿# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 Ague Samuel Amen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 """
 BCASL loader (simplifié)
