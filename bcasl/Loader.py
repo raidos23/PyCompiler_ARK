1b9083dc3a2747a4af90e680b0942cc876979a63
Samuel Amen Ague <ague.samuel27@gmail.com>
2026-05-16 16:41:00 +0000

    correctif mineur

--- bcasl/Loader.py
+++ bcasl/Loader.py
@@ -478,7 +478,7 @@ def _load_workspace_config(workspace_root: Path) -> dict[str, Any]:
         plugin_timeout = 0.0
 
         try:
-            from Core.ArkConfigManager import load_ark_config
+            from Core.ArkConfig import load_ark_config
 
             ark_config = load_ark_config(str(workspace_root))
 
