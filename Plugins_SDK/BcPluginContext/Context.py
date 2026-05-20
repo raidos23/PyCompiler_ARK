6aec2127a6d19f9b3c8a0ddb9215a00be7ef7558
Samuel Amen Ague <ague.samuel27@gmail.com>
2026-05-20 09:11:32 +0000

    refactor: déplacemnt de la logique de permission special vers service et  remise du manager de cfg d'engine dans Core/engine

--- Plugins_SDK/BcPluginContext/Context.py
+++ Plugins_SDK/BcPluginContext/Context.py
@@ -288,7 +288,7 @@ def set_selected_workspace(path: Pathish) -> bool:
         pass
     # Try to inform the GUI when running with UI; ignore result and accept by contract
     try:
-        from Core.Api import request_workspace_change_from_BcPlugin  # type: ignore
+        from Services.AdvancedAuth import request_workspace_change_from_BcPlugin  # type: ignore
 
         try:
             request_workspace_change_from_BcPlugin(str(path))
