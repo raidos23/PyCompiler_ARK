4b7e1a9ba3d950f52488967e1c67cafdae9531e7
Samuel Amen Ague <ague.samuel27@gmail.com>
2026-06-02 17:03:52 +0000

    chore: incrémentation des versions des moteurs à v1.1.0 suite aux ajouts fonctionnels Signed-off-by: Samuel Amen Ague <ague.samuel27@gmail.com>

--- engines/pyinstaller/__init__.py
+++ engines/pyinstaller/__init__.py
@@ -51,7 +51,7 @@ class PyInstallerEngine(CompilerEngine):
 
     id: str = "pyinstaller"
     name: str = "PyInstaller"
-    version: str = "1.0.0"
+    version: str = "1.1.0"
     required_core_version: str = "1.0.0"
     required_sdk_version: str = "1.0.0"
 
